7a66a93fab8dd8f43f7936dd2fc86ae836d10901
DevRogue20 <74317830+DevRogue20@users.noreply.github.com>
2022-02-24 03:29:55 -0600

    JS Update 1

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 
 var gamePattern = [];
-
 var buttonColors = ["red", "blue", "green", "yellow"];
 
-var randomChosenColor = buttonColors[nextSequence()];
-
-gamePattern.push(randomChosenColor);
+$(".btn").click(function(){
+  var randomChosenColor = buttonColors[nextSequence()];
+  gamePattern.push(randomChosenColor);
+});
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * (3 + 1));
