0218f1c020f6ec52fb067860aa53c34c7cd3c1e5
DevRogue20 <74317830+DevRogue20@users.noreply.github.com>
2022-02-24 00:37:09 -0600

    initial JS setup

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
 
-var gamePattern = [""];
+var gamePattern = [];
 
 var buttonColors = ["red", "blue", "green", "yellow"];
 
 var randomChosenColor = buttonColors[nextSequence()];
+
+gamePattern.push(randomChosenColor);
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * (3 + 1));
